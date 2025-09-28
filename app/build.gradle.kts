4473e235738e1778ad9d06b67153e7fd1056a67c
pablo <pabloarmasbosse@gmail.com>
2025-09-28 17:28:36 +0200

    Cambiado el nombre del proyecto.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.calculadora"
+        applicationId = "com.example.topo"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
